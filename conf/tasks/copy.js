18f62a91ea661b7b4cc2f429e71102060179c516
45kb <filippo.oretti@gmail.com>
2016-06-25 12:57:11 +0200

    sass folder renamed to scss

--- conf/tasks/copy.js
+++ conf/tasks/copy.js
@@ -10,6 +10,6 @@
   gulp.task('copy', function copySrc() {
 
     return gulp.src([`${paths.scss}*`])
-    .pipe(gulp.dest(`${paths.dist}sass`));
+    .pipe(gulp.dest(`${paths.dist}scss`));
   });
 }());
