82231991244635fc0c2db5b44edbaacdb80eb0a9
45kb <filippo.oretti@gmail.com>
2016-06-26 09:45:48 +0200

    gulp-autoprefixer task done, wondering if it is good to release without a mixin.scss file ...

--- conf/tasks/sass.js
+++ conf/tasks/sass.js
@@ -5,13 +5,11 @@
   const gulp = require('gulp')
     , paths = require('../paths')
     , gulpSass = require('gulp-sass')
-    , sourcemaps = require('gulp-sourcemaps');
+    , autprefixer = require('gulp-autoprefixer');
 
   gulp.task('sass', function sassify() {
     return gulp.src(`${paths.lib}scss/!(mixins).scss`)
-    .pipe(sourcemaps.init())
     .pipe(gulpSass())
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest(`${paths.dist}`));
   });
 }());
